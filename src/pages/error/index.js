9dc537e9d35bb4cf0a70f6e858997a7ae715d6ac
unknown <themtrx@mail.bg>
2020-08-08 19:04:38 +0300

    feat: error page

---
+++ src/pages/error/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Wrapper from '../wrapper'
+
+const ErrorPage = () => {
+    return (
+        <Wrapper>
+            <div>Error...</div>
+        </Wrapper>
+    )
+}
+
+export default ErrorPage
